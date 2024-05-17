c2e9caec97ee502f457e90a0a56a565f58db39cb
null5506 <digisparkuser.5506@gmail.com>
2024-05-17 14:38:16 -0300

    Adicionado a aba de registro, refinado o login e criado a aba perfil

--- scienceglobe/scienceglobe/urls.py
+++ scienceglobe/scienceglobe/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import RedirectView
+from user import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', RedirectView.as_view(url='home/', permanent= True) ),
@@ -27,4 +28,5 @@ urlpatterns = [
     path('noticias/', include('noticias.urls')),
     path('foruns/', include('foruns.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('user/', include('user.urls'))
 ]
