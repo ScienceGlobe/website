5bab867ab5fcafaab955261c0e77f45d0a7d2335
zinid <88515936+ZiiniD@users.noreply.github.com>
2024-05-20 15:53:05 -0300

    complemento para a pagina home
    calcula quantidade de noticias de cada genero

--- scienceglobe/home/views.py
+++ scienceglobe/home/views.py
@@ -4,21 +4,42 @@ from django.http import HttpResponse
 
 
 Artigo = apps.get_model('artigos', 'Artigo')
-Noticia = apps.get_model('noticias', 'Noticia')
-Autor = apps.get_model('artigos', 'Autor')
+NoticiaWeb = apps.get_model('noticias', 'NoticiaWeb')
+Genero = apps.get_model('noticias', 'Genero')
 Forum = apps.get_model('foruns', 'ForumPost')
 
 def home_view(request):
 
+    genero_economia = Genero.objects.get(name='Economia')
+    genero_tecnologia = Genero.objects.get(name='Tecnologia')
+    genero_meioAmbiente = Genero.objects.get(name='Meio Ambiente')
+    genero_ciencias = Genero.objects.get(name='Ciências')
+
+
+
+
     num_artigos = Artigo.objects.all().count()
-    num_noticias = Noticia.objects.all().count()
-    num_autores = Autor.objects.count()
+
+
+
+    num_noticias = NoticiaWeb.objects.count()
+    num_economia = NoticiaWeb.objects.filter(genero=genero_economia).count()
+    num_tecnologia = NoticiaWeb.objects.filter(genero=genero_tecnologia).count()
+    num_ciencias = NoticiaWeb.objects.filter(genero=genero_ciencias).count()
+    num_meioambiente = NoticiaWeb.objects.filter(genero=genero_meioAmbiente).count()
+    
+
+
+
     num_foruns = Forum.objects.all().count()
 
     context = {
         'num_artigos': num_artigos,
         'num_noticias': num_noticias,
-        'num_autores': num_autores,
+        'num_economia': num_economia,
+        'num_tecnologia': num_tecnologia,
+        'num_meioambiente': num_meioambiente,
+        'num_ciencias': num_ciencias,
         'num_foruns': num_foruns,
     }
     return render(request, "home.html", context=context)
