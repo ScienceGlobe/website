64522ee3c99ea7d1bef6ea909aaeda35f7dfe321
zinid <88515936+ZiiniD@users.noreply.github.com>
2024-05-24 19:42:06 -0300

    Views de home atualizado
    - agora calcula quantidade de foruns pra cada genero

--- scienceglobe/home/views.py
+++ scienceglobe/home/views.py
@@ -7,6 +7,7 @@ Artigo = apps.get_model('artigos', 'Artigo')
 NoticiaWeb = apps.get_model('noticias', 'NoticiaWeb')
 Genero = apps.get_model('noticias', 'Genero')
 Forum = apps.get_model('foruns', 'ForumPost')
+ForumGenero = apps.get_model("foruns", "GeneroForum")
 
 def home_view(request):
 
@@ -14,6 +15,13 @@ def home_view(request):
     genero_tecnologia = Genero.objects.get(name='Tecnologia')
     genero_meioAmbiente = Genero.objects.get(name='Meio Ambiente')
     genero_ciencias = Genero.objects.get(name='Ciências')
+
+
+    forum_genero_duvidas = ForumGenero.objects.get(name= 'Duvidas')
+    forum_genero_tecnologia = ForumGenero.objects.get(name= 'Tecnologia')
+    forum_genero_curiosidades = ForumGenero.objects.get(name= 'Curiosidades')
+    forum_genero_ciencias = ForumGenero.objects.get(name= 'Ciências')
+    forum_genero_outros = ForumGenero.objects.get(name= 'Outros')
 
 
 
@@ -32,6 +40,11 @@ def home_view(request):
 
 
     num_foruns = Forum.objects.all().count()
+    num_foruns_curiosidades = Forum.objects.filter(genero=forum_genero_curiosidades).count()
+    num_foruns_tecnologia = Forum.objects.filter(genero=forum_genero_tecnologia).count()
+    num_foruns_ciencias = Forum.objects.filter(genero =forum_genero_ciencias).count()
+    num_foruns_duvidas = Forum.objects.filter(genero=forum_genero_duvidas).count()
+    num_foruns_outros = Forum.objects.filter(genero=forum_genero_outros).count()
 
     context = {
         'num_artigos': num_artigos,
@@ -41,5 +54,10 @@ def home_view(request):
         'num_meioambiente': num_meioambiente,
         'num_ciencias': num_ciencias,
         'num_foruns': num_foruns,
+        'num_foruns_curiosidades': num_foruns_curiosidades,
+        'num_foruns_tecnologia': num_foruns_tecnologia,
+        'num_foruns_ciencias': num_foruns_ciencias,
+        'num_foruns_duvidas': num_foruns_duvidas,
+        'num_foruns_outros': num_foruns_outros,
     }
     return render(request, "home.html", context=context)
