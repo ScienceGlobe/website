f977517f05641c741fef8dfa4deb4a47543af768
null <64292548+NULL5506@users.noreply.github.com>
2024-05-31 19:25:00 -0300

    Delete scienceglobe directory

--- scienceglobe/foruns/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class ForunsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'foruns'
-    def ready(self):
-        print("Apps de foruns inicializado.")
-        from . import signals
