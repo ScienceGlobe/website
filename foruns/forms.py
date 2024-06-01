3e32aa77fe4dd2269aab00ce7d688ec2045c355c
null <64292548+NULL5506@users.noreply.github.com>
2024-05-31 21:13:31 -0300

    Delete foruns directory

--- foruns/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import ForumPost
-
-class ForumPostForm(forms.ModelForm):
-    class Meta:
-        model = ForumPost
-        fields = ['name', 'genero','description']
