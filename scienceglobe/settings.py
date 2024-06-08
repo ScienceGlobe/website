444482dc80259c452bdb58e89b360c62daa1b7e7
null <64292548+NULL5506@users.noreply.github.com>
2024-06-07 21:50:37 -0300

    update

--- scienceglobe/settings.py
+++ scienceglobe/settings.py
@@ -14,6 +14,7 @@ import os
 from pathlib import Path
 import django_heroku
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5f1@z=kjc+#cy8+6^2m^09w5)*(am6i*k5dl^7gf)-=%4om^m4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['sciglobe-c1661e85771c.herokuapp.com']
 
